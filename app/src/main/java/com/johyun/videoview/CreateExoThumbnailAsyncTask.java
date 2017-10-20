9a25debbfb9ebeca74565bcfc62cad120c62b8a8
JoHyun <hyun@wemoss.com>
2017-10-20 17:03:17 +0900

    add exoPlayer thumbnail task exoPlayer 로직 수정

---
+++ app/src/main/java/com/johyun/videoview/CreateExoThumbnailAsyncTask.java
@@ -0,0 +1,63 @@
+package com.johyun.videoview;
+
+import android.graphics.Bitmap;
+import android.media.MediaMetadataRetriever;
+import android.os.AsyncTask;
+import android.util.Log;
+
+import java.util.HashMap;
+
+/**
+ * Created by JH on 2017. 10. 18..
+ */
+
+public class CreateExoThumbnailAsyncTask extends AsyncTask<String, Void, Boolean> {
+
+    private static final String TAG = CreateExoThumbnailAsyncTask.class.getSimpleName();
+
+    public interface CreateExoThumbnailCompleteListener {
+        void onCreateComplete(Bitmap bitmap);
+    }
+
+    private CreateExoThumbnailCompleteListener createExoThumbnailCompleteListener;
+    private Bitmap bitmap;
+
+
+    public CreateExoThumbnailAsyncTask(CreateExoThumbnailCompleteListener createExoThumbnailCompleteListener) {
+        this.createExoThumbnailCompleteListener = createExoThumbnailCompleteListener;
+    }
+
+    @Override
+    protected Boolean doInBackground(String... strings) {
+
+        Log.d(TAG, "CreateThumbnailAsyncTask doInBackground");
+
+        MediaMetadataRetriever mediaMetadataRetriever = null;
+        try {
+            mediaMetadataRetriever = new MediaMetadataRetriever();
+            mediaMetadataRetriever.setDataSource(strings[0], new HashMap<String, String>());
+            bitmap = mediaMetadataRetriever.getFrameAtTime(10000);
+
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (mediaMetadataRetriever != null) {
+                mediaMetadataRetriever.release();
+            }
+        }
+
+        return false;
+    }
+
+    @Override
+    protected void onPostExecute(Boolean result) {
+        super.onPostExecute(result);
+
+        if (result) {
+            Log.d(TAG, "CreateThumbnailAsyncTask onPostExecute");
+            createExoThumbnailCompleteListener.onCreateComplete(bitmap);
+        }
+    }
+}
+
--- app/src/main/java/com/johyun/videoview/ExoVideoPlayer.java
+++ app/src/main/java/com/johyun/videoview/ExoVideoPlayer.java
@@ -3,22 +3,24 @@ package com.johyun.videoview;
 import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.res.Resources;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Build;
 import android.util.AttributeSet;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 
-import com.google.android.exoplayer2.DefaultLoadControl;
 import com.google.android.exoplayer2.ExoPlaybackException;
 import com.google.android.exoplayer2.ExoPlayer;
 import com.google.android.exoplayer2.ExoPlayerFactory;
-import com.google.android.exoplayer2.LoadControl;
+import com.google.android.exoplayer2.PlaybackParameters;
+import com.google.android.exoplayer2.Player;
 import com.google.android.exoplayer2.SimpleExoPlayer;
 import com.google.android.exoplayer2.Timeline;
 import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
@@ -26,7 +28,7 @@ import com.google.android.exoplayer2.extractor.ExtractorsFactory;
 import com.google.android.exoplayer2.source.ExtractorMediaSource;
 import com.google.android.exoplayer2.source.MediaSource;
 import com.google.android.exoplayer2.source.TrackGroupArray;
-import com.google.android.exoplayer2.trackselection.AdaptiveVideoTrackSelection;
+import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
 import com.google.android.exoplayer2.trackselection.TrackSelection;
 import com.google.android.exoplayer2.trackselection.TrackSelectionArray;
@@ -38,8 +40,6 @@ import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
 import com.google.android.exoplayer2.util.Util;
 
-import static com.johyun.videoview.R.id.exo_player;
-
 /**
  * Created by JH on 2017. 10. 19..
  */
@@ -49,14 +49,19 @@ public class ExoVideoPlayer extends RelativeLayout implements ExoPlayer.EventLis
     private final static String TAG = ExoVideoPlayer.class.getSimpleName();
     private Context context;
 
-    private RelativeLayout rl_exo_player_wrapper, rl_exo_player_controller_wrapper;
-    private ImageView iv_exo_player_full_screen, iv_exo_player_share;
+    private RelativeLayout rl_exo_player_wrapper;
+    private ImageView iv_exo_player_full_screen;
+    private ImageView iv_exo_player_share;
+    private ImageView iv_custom_video_sound;
     private ProgressBar pb_exo_player_progressBar;
 
     private SimpleExoPlayer exoPlayer;
     private SimpleExoPlayerView playerView;
-//    private String tempUrl = "http://www.sample-videos.com/video/mp4/720/big_buck_bunny_720p_1mb.mp4";
+
+    private boolean isPlayerPlaying;
+
     private String tempUrl = "https://www.rmp-streaming.com/media/bbb-360p.mp4";
+
 
     public ExoVideoPlayer(Context context) {
         super(context);
@@ -82,9 +87,9 @@ public class ExoVideoPlayer extends RelativeLayout implements ExoPlayer.EventLis
     private void initUi() {
         rl_exo_player_wrapper = findViewById(R.id.rl_exo_player_wrapper);
         pb_exo_player_progressBar = findViewById(R.id.pb_exo_player_progressBar);
-        rl_exo_player_controller_wrapper = findViewById(R.id.rl_exo_player_controller_wrapper);
         iv_exo_player_full_screen = findViewById(R.id.iv_exo_player_full_screen);
         iv_exo_player_share = findViewById(R.id.iv_exo_player_share);
+        iv_custom_video_sound = findViewById(R.id.iv_custom_video_sound);
     }
 
     private void initPlayer(final Context context) {
@@ -96,26 +101,71 @@ public class ExoVideoPlayer extends RelativeLayout implements ExoPlayer.EventLis
         pb_exo_player_progressBar.setVisibility(VISIBLE);
 
         BandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
-        TrackSelection.Factory videoTrackSelectionFactory = new AdaptiveVideoTrackSelection.Factory(bandwidthMeter);
+        TrackSelection.Factory videoTrackSelectionFactory = new AdaptiveTrackSelection.Factory(bandwidthMeter);
         TrackSelector trackSelector = new DefaultTrackSelector(videoTrackSelectionFactory);
-        LoadControl loadControl = new DefaultLoadControl();
-
-        exoPlayer = ExoPlayerFactory.newSimpleInstance(context, trackSelector, loadControl);
-        playerView = findViewById(exo_player);
-        playerView.setPlayer(exoPlayer);
-        playerView.setKeepScreenOn(true);
 
         DataSource.Factory dataSourceFactory = new DefaultDataSourceFactory(context, Util.getUserAgent(context, "ExoPlayer"));
         ExtractorsFactory extractorsFactory = new DefaultExtractorsFactory();
         MediaSource videoSource = new ExtractorMediaSource(Uri.parse(tempUrl), dataSourceFactory, extractorsFactory, null, null);
 
+        exoPlayer = ExoPlayerFactory.newSimpleInstance(context, trackSelector);
         exoPlayer.prepare(videoSource);
         exoPlayer.addListener(this);
-        playerView.requestFocus();
         exoPlayer.setPlayWhenReady(false);
-        playerView.setUseController(false);// 콘트롤러 사용 못함
 
-        //todo 콘트롤러 UI & 로직 추가
+        playerView = findViewById(R.id.exo_player);
+        playerView.setPlayer(exoPlayer);
+        playerView.setKeepScreenOn(true);
+        playerView.requestFocus();
+        playerView.setUseArtwork(true);
+
+        createThumbnail();
+
+        //todo stop video when video view out of screen
+
+        // 풀스크린 클릭리스너
+        iv_exo_player_full_screen.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "onClick FULL SCREEN");
+                //todo full screen logic
+            }
+        });
+
+        // 공유 클릭리스너
+        iv_exo_player_share.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "onClick SHARE");
+                //todo share logic
+            }
+        });
+
+        // 사운드 클릭리스너
+        iv_custom_video_sound.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "onClick SOUND");
+                if (exoPlayer.getVolume() == 0) {
+                    exoPlayer.setVolume(1);
+                    view.setAlpha(1.0f);
+                } else {
+                    exoPlayer.setVolume(0);
+                    view.setAlpha(0.6f);
+                }
+            }
+        });
+    }
+
+    //todo bitmap 적용 안됨
+    private void createThumbnail() {
+        new CreateExoThumbnailAsyncTask(new CreateExoThumbnailAsyncTask.CreateExoThumbnailCompleteListener() {
+            @Override
+            public void onCreateComplete(Bitmap bitmap) {
+                Log.d(TAG, "createThumbnail bitmap = " + bitmap);
+                playerView.setDefaultArtwork(bitmap);
+            }
+        }).execute(tempUrl);
     }
 
     @Override
@@ -139,24 +189,57 @@ public class ExoVideoPlayer extends RelativeLayout implements ExoPlayer.EventLis
     public void onPlayerStateChanged(boolean playWhenReady, int playbackState) {
 
         switch (playbackState) {
-            case ExoPlayer.STATE_BUFFERING:
+            case Player.STATE_BUFFERING:
                 Log.d(TAG, "onPlayerStateChanged STATE_BUFFERING");
                 pb_exo_player_progressBar.setVisibility(VISIBLE);
                 break;
-            case ExoPlayer.STATE_IDLE:
+            case Player.STATE_IDLE:
                 Log.d(TAG, "onPlayerStateChanged STATE_IDLE");
                 pb_exo_player_progressBar.setVisibility(VISIBLE);
                 break;
-            case ExoPlayer.STATE_READY:
+            case Player.STATE_READY:
                 Log.d(TAG, "onPlayerStateChanged STATE_READY");
                 pb_exo_player_progressBar.setVisibility(GONE);
                 break;
-            case ExoPlayer.STATE_ENDED:
+            case Player.STATE_ENDED:
                 Log.d(TAG, "onPlayerStateChanged STATE_ENDED");
                 // 처음으로 돌아가서 정지
                 exoPlayer.seekTo(0);
                 exoPlayer.setPlayWhenReady(false);
                 break;
+        }
+    }
+
+    @Override
+    public void onPlaybackParametersChanged(PlaybackParameters playbackParameters) {
+        Log.d(TAG, "onPlaybackParametersChanged playbackParameters = " + playbackParameters);
+    }
+
+    @Override
+    public void onRepeatModeChanged(int repeatMode) {
+        Log.d(TAG, "onRepeatModeChanged repeatMode = " + repeatMode);
+    }
+
+    // exoPlayer 해제
+    public void releaseExoPlayer(){
+        if(exoPlayer != null) {
+            exoPlayer.release();
+        }
+        exoPlayer = null;
+    }
+
+    // 백그라운드로 내려갈때
+    public void goToBackground(){
+        if(exoPlayer != null){
+            isPlayerPlaying = exoPlayer.getPlayWhenReady();
+            exoPlayer.setPlayWhenReady(false);
+        }
+    }
+
+    //포어그라운드로 올라올때
+    public void goToForeground(){
+        if(exoPlayer != null){
+            exoPlayer.setPlayWhenReady(isPlayerPlaying);
         }
     }
 
